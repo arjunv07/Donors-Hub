1eecc810f8a8c8171c304ae47a1a91e4997791b4
arjunv07 <arjunvijayanathakurup@gmail.com>
2017-11-03 20:30:10 +0530

    Modifies

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+// ****************************************************************
 // Application Packages required
+// ****************************************************************
 var express = require("express"),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
@@ -6,7 +8,9 @@ var express = require("express"),
     expressSanitizer = require("express-sanitizer"),
     app = express();
 
+// ****************************************************************
 // App configuration
+// ****************************************************************
 mongoose.connect("mongodb://localhost/donorshub");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -14,50 +18,92 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
-// TODO: 
-/*var donorSchema = new mongoose.Schema({
-    name: String,
-    mobile: String,
-    gender: String,
-});*/ //TODO : Complete schema
-
-// var DonorUser
-
-// Index page
+// ****************************************************************
+    // Index page...(Static)
+// ****************************************************************    
 app.get("/", function(req, res){
-    res.render("index");
+    res.render("static/index");
 });
 
-//
+// ****************************************************************
+    // About Page...(Static)
+// ****************************************************************    
 app.get("/about", function (req, res) {
-    res.render("about");
+    res.render("static/about");
 });
 
+// ****************************************************************
+    // Who can Help Page...(Static)
+// ****************************************************************
 app.get("/whocanhelp", function (req, res) {
-    res.render("help");
+    res.render("static/help");
 });
 
+// ****************************************************************
+    // Did you know Page...(Static)
+// ****************************************************************
 app.get("/fact", function (req, res) {
-    res.render("did_you_know");
+    res.render("static/did_you_know");
 });
 
+// ****************************************************************
+    // Donation Type Page...(Static)
+// ****************************************************************
 app.get("/donation-type", function (req, res) {
-    res.render("donation_type");
+    res.render("static/donation_type");
 });
 
+// ****************************************************************
+    // Ethnic Blood-Group Page...(Static)
+// ****************************************************************
 app.get("/ethnic-group", function(req, res){
-    res.render("ethnic_group");
+    res.render("static/ethnic_group");
 });
 
+// ****************************************************************
+    // Tips Page...(Static)
+// ****************************************************************
 app.get("/tips", function (req, res) {
-    res.render("tips");
+    res.render("static/tips");
 });
 
+// ****************************************************************
+    // Gallery Page...(Dynamic)
+// ****************************************************************
 app.get("/gallery", function (req, res) {
-    res.render("gallery");
+    res.render("static/gallery");
 });
 
-// Server Configuration
+// ****************************************************************
+    // Admin Panel Page...(Master control and Dynamic)
+// ****************************************************************
+app.get("/admin", function(req, res){
+    res.render("static/admin");
+    console.log("Request for Admin Panel...");
+}); 
+
+// ****************************************************************
+    // Request Page... (Dynamic) 
+// ****************************************************************
+// TODO: request page
+app.get("/requests", function(req, res){
+    res.send("Request page");
+});
+
+// TODO: Available people
+
+// ****************************************************************
+    // For unavailable URL requests
+// ****************************************************************
+app.get("*", function (req, res) {
+    res.send("Sorry page not available!! Please check your URL");
+    res.render("static/404-page");
+    // TODO: 404-PAGE RENDERING
+});
+
+// ****************************************************************
+    // Server Configuration
+// ****************************************************************
 app.listen("3000", function(){
   console.log("Server Running!!");
 });
