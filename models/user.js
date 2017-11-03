8a100063866b984f983447a216c490c0012510de
arjunv07 <arjunvijayanathakurup@gmail.com>
2017-11-03 20:14:51 +0530

    Added models File directory

---
+++ models/user.js
@@ -0,0 +1,10 @@
+// ****************************************************************
+    // MongoDb Schema
+// ****************************************************************    
+// TODO: 
+var donorSchema = new mongoose.Schema({
+    name: String,
+    mobile: String,
+    gender: String,
+});
+ //TODO : Complete schema
