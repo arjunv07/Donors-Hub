914c655d2995d2b7e9c2c7f18e27bfc3c71698e3
arjunv07 <arjunvijayanathakurup@gmail.com>
2017-11-06 07:16:41 +0530

    Added router

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var express = require("express"),
     mongoose = require("mongoose"),
     methodOverride = require("method-override"),
     expressSanitizer = require("express-sanitizer"),
+    donor = require("./routes/donor"),
+    static = require("./routes/static"),
+    recepient = require("./routes/donor");
     Recepient = require("./models/recepient"),
     Donor = require("./models/donor"),
     app = express();
@@ -22,157 +25,74 @@ app.use(methodOverride("_method"));
 
 // ****************************************************************
     // Index page...(Static)
-// ****************************************************************    
-app.get("/", function(req, res){
-    res.render("static/index");   
-});
-
+// **************************************************************** 
+app.use("/", static);
+   
 // ****************************************************************
     // About Page...(Static)
 // ****************************************************************    
-app.get("/about", function (req, res) {
-    res.render("static/about");
-});
+app.use("/about", static);
 
 // ****************************************************************
     // Who can Help Page...(Static)
 // ****************************************************************
-app.get("/whocanhelp", function (req, res) {
-    res.render("static/help");
-});
+app.use("whocanhelp", static);
 
 // ****************************************************************
     // Did you know Page...(Static)
 // ****************************************************************
-app.get("/fact", function (req, res) {
-    res.render("static/did_you_know");
-});
+app.get("/fact", static);
 
 // ****************************************************************
     // Donation Type Page...(Static)
 // ****************************************************************
-app.get("/donation-type", function (req, res) {
-    res.render("static/donation_type");
-});
+app.get("/donation-type", static);
 
 // ****************************************************************
     // Ethnic Blood-Group Page...(Static)
 // ****************************************************************
-app.get("/ethnic-group", function(req, res){
-    res.render("static/ethnic_group");
-});
+app.get("/ethnic-group", static);
 
 // ****************************************************************
     // Tips Page...(Static)
 // ****************************************************************
-app.get("/tips", function (req, res) {
-    res.render("static/tips");
-});
+app.get("/tips", static);
 
 // ****************************************************************
     // Gallery Page...(Dynamic)
 // ****************************************************************
-app.get("/gallery", function (req, res) {
-    res.render("static/gallery");
-});
+app.get("/gallery", static);
 
 // ****************************************************************
     // Admin Panel Page...(Master control and Dynamic)
 // ****************************************************************
-app.get("/admin", function(req, res){
-    res.render("static/admin");
-    console.log("Request for Admin Panel...");
-}); 
+app.get("/admin", static); 
 
 // ****************************************************************
     // Request Page... (Dynamic) 
 // ****************************************************************
-app.get("/donor", function(req, res){
-    Donor.find({}, function(err, alldonors){
-        if(err){
-            console.log(err);
-        }else{
-            res.render("donor/index", {donors: alldonors});
-        }
-    });
-});
+app.get("/donor",donor);
 
 // ****************************************************************
 // Donor's Details...(Dynamic)
 // ****************************************************************
-app.post("/donor", function(req, res){
-    var name = req.body.name;
-    var email = req.body.email;
-    var password = req.body.password;
-    var mobile = req.body.mobile;
-    var age = req.body.age;
-    var gender = req.body.gender;
-    var bloodgroup = req.body.bloodgroup;
-    var available = req.body.available;
-    var newDonor = {name: name, email: email, password: password, mobile: mobile, age: age, gender: gender, bloodgroup: bloodgroup, available: available};
-    // console.log(name + email + password + mobile + age + gender + bloodgroup + available);
-    Donor.create(newDonor, function(err, pushed){
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.redirect(301, "/donor");
-        }
-    });
-});
-app.get("/donor/show", function (req, res) {
-    // Donor.findById(req.params.id).populate("donors").exec(function(err, founddonor){
-    //     if(err){
-    //         console.log(err);
-    //     }
-    //     else{
-    res.render("donor/show");//, {donor: founddonor});
-    // }
-    // });
-});
+app.post("/donor", donor);
+
+app.get("/donor/show",donor);
 // ****************************************************************
 // Form for adding Donor's Detail
 // ****************************************************************
-app.get("/new", function (req, res) {
-    res.render("donor/new");
-});
+app.get("/new", donor);
 
 
 // ****************************************************************
 // For request Page...(Dynamic)
 // ****************************************************************
-app.get("/recepient", function(req, res){
-    Recepient.find({}, function (err, allrecepient) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("recepient/index", { recepients: allrecepient })
-        }
-    });
-});
+app.get("/recepient", recepient);
 
-app.post("/recepient", function(req, res){
-    var name = req.body.name;
-    var email = req.body.email;
-    var mobile = req.body.mobile;
-    var bloodgroup = req.body.bloodgroup;
-    var age = req.body.age;
-    var gender = req.body.gender;
-    var place = req.body.place;
-    var time = req.body.time;
-    var newRecepient = {name: name, email: email, mobile: mobile, bloodgroup: bloodgroup, age: age, gender: gender, place: place, time: time}
-    Recepient.create(newRecepient, function(err, pushed){
-        if(err){
-            console.log(err);
-        }else{
-            res.redirect(301, "/recepient");
-        }
-    });
-});
+app.post("/recepient", recepient);
 
-app.get("/newr", function(req, res){
-    res.render("recepient/new");
-});
+app.get("/newr", recepient);
 
 // app.get("/recepient/:id", function () {
 //     Donor.findById(req.params.id).populate("recepients").exec(function (err, foundrecepient) {
